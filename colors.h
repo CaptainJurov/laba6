eaeab689bcd8738cd157370119cbfa32a6558289
CaptainJurov <goudazv@gmail.com>
2024-11-18 08:11:27 +0700

    Первый

---
+++ colors.h
@@ -0,0 +1,15 @@
+#pragma once
+namespace Colors {
+    const char * red = "\033[1;31m";
+    const char * green = "\033[1;32m";
+    const char * yellow = "\033[1;33m";
+    const char * blue = "\033[1;34m";
+    const char * reset = "\033[0m";
+    const char * bold = "\033[1m";
+    const char * ital = "\033[3m";
+    const char * конец_ital = "\033[23m";
+    const char * зачёрк = "\033[21m";
+    const char * недозелёный = "\033[38;2;170;230;170m";
+    const char * скучный = "\033[38;2;130;130;130m";
+}
+//https://ru.wikipedia.org/wiki/%D0%A3%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D1%8F%D1%8E%D1%89%D0%B8%D0%B5_%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8_ANSI
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <stack>
+#include <string>
+#include "colors.h"
+const char glas[] = "aAeEiIoOuU";
+bool match(char s) {
+    for (auto i: glas) {
+        if (s==i) {return true;}
+    }
+    return false;
+} 
+struct Источник {
+    std::stack<char> Input;
+    Источник () {}
+    virtual void str_to_stack(std::string& str) {
+        for (auto i: str) {
+            Input.push(i);
+        }
+    }
+    std::string get_str() {
+        std::string temp;
+        std::stack<char> temps = Input;
+        while (!temps.empty()) {
+            temp.push_back(temps.top());
+            temps.pop();
+        }
+        return temp;
+    }
+};
+
+struct Второй: Источник {
+    Второй(): Источник() {}
+    void str_to_stack(std::string& str) override {
+        for (auto i: str) {
+            if (match(i)) {
+                Input.push(i);
+            }
+        }
+    }
+    Второй(Источник& gl): Источник() {
+        std::string str = gl.get_str();
+        str_to_stack(str);
+    }
+};
+
+class Коробка {
+private:
+    Источник Первый;
+    Второй Приёмник;
+public:
+    Источник& get_First() {
+        return Первый;
+    }
+    Второй& get_Second() {
+        return Приёмник;
+    }
+    friend std::istream& operator>>(std::istream& is, Коробка& источник) {
+        std::string temp;
+        is >> temp;
+        источник.get_First().str_to_stack(temp);
+        источник.get_Second().str_to_stack(temp);
+        return is;
+    }
+};
+
+int main() {
+    std::cout<<Colors::зачёрк<<"Вводи текс: ";
+    Коробка хз;
+    std::cin >> хз;
+    std::cout << Colors::reset;
+    std::cout << "Первый стак: " << Colors::green << Colors::bold << хз.get_First().get_str() << std::endl << Colors::reset;
+    std::cout << "Второй стак(с гласными): " << Colors::blue << Colors::bold << хз.get_Second().get_str() << std::endl << Colors::reset;
+    return 0;
+}
